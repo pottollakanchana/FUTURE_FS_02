6ae9d60194572bca7b29a248e130343310755e52
pottollakanchana <kanchana.infotech99@gmail.com>
2026-06-03 12:31:26 +0530

    Update db.js

--- CRM/server/config/db.js
+++ CRM/server/config/db.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
-    const connString = 'mongodb+srv://kanchana:kanchana123@cluster0.v9xjl.mongodb.net/crm?retryWrites=true&w=majority';
-    await mongoose.connect(connString);
-    console.log('MongoDB Connected Successfully');
-  } catch (error) {
-    console.error('Database connection failed:', error.message);
-    process.exit(1);
-  }
+    try {
+        const conn = await mongoose.connect('mongodb+srv://public_user:internship2026@cluster0.v9xjl.mongodb.net/crm?retryWrites=true&w=majority');
+        console.log(MongoDB Connected: ${conn.connection.host});
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
